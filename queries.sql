ab06c4ec5f7d4d6445401e25443fdbc0c6a50ad6
Yumi Kimura <ym19.kimura@gmail.com>
2021-12-04 19:57:59 -0800

    7.3.4 adding

--- queries.sql
+++ queries.sql
@@ -17,9 +17,9 @@ CREATE TABLE employees (
 );
 
 
-CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
+CREATE TABLE dept_emp (
+   emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
@@ -92,10 +92,56 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 
-SELECT first_name, last_name
+-- Create new table for retiring employees
+SELECT emp_no,first_name, last_name
 INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
+-- Check the table
 SELECT * FROM retirement_info;
+
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+retirement_info.last_name,
+   pt_em dep.to_date	
+	
+FROM retirement_info 
+LEFT JOIN dept_emp 
+ON retirement_info.emp_no = dept_emp.emp_no; 
+
+-- shotern names
+SELECT ri.emp_no,
+    ri.first_name,
+ri.last_name,
+    de.to_date
+
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+
+
+--use left join for retirement_info and dept_emp tables
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+SELECT * FROM current_emp
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
